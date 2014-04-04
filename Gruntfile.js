ea4dfed3a496141dc926856f59518dcc41fb6f4b
Ben Drucker <bvdrucker@gmail.com>
2014-04-04 14:15:34 -0400

    Page layout and build process

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('assemble');
+  grunt.initConfig({
+    assemble: {
+      options: {
+        plugins: [
+          'assemble-contrib-permalinks',
+          'handlebars-helper-partial'
+        ],
+        layoutdir: './layouts',
+        layoutext: '.hbs',
+        layout: 'default'
+      },
+      posts: {
+        options: {
+          layout: 'post',
+          engine: 'handlebars',
+          permalinks: {
+            structure: ':basename/index.html',
+          },
+        },
+        files: [{
+          expand: true,
+          cwd: './posts',
+          src: ['**/*.md'],
+          dest: './build/posts',
+          ext: '.html'
+        }]
+      }
+    }
+  });
+};
