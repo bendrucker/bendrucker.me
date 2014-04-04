476e1068ae7a5cb11b017a65ebd2fa6d1c5cacc1
Ben Drucker <bvdrucker@gmail.com>
2014-04-04 11:45:09 -0400

    Basic style operations

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,20 @@ gulp.task('clean', function () {
   return gulp.src(build, {read: false})
     .pipe(plugins.clean());
 });
+
+gulp.task('styles:development', function () {
+  return gulp.src('./styles/*.styl')
+    .pipe(plugins.stylus({
+      use: ['nib']
+    }))
+    .pipe(gulp.dest(build + '/styles'));
+});
+
+gulp.task('styles:production', function () {
+  return gulp.src('./styles/main.styl')
+    .pipe(plugins.stylus({
+      use: ['nib'],
+      set: ['compress']
+    }))
+    .pipe(gulp.dest(build + '/styles'));
+});
