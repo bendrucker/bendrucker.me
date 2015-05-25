071b756536a76615fc9af57c78a33e64dc76f63f
Ben Drucker <bvdrucker@gmail.com>
2015-05-25 07:19:33 -0400

    Render post item

---
+++ src/post/list.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import BaseElement from 'base-element'
+import ListItem from './list-item'
+
+export default class PostList extends BaseElement {
+  render (posts) {
+    this.afterRender(this.html('div.posts', posts.map(post => new ListItem().render(post))))
+  }
+}
