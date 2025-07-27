74d3991e1e8aa56d0e56630558f75cad806c7737
Ben Drucker <bvdrucker@gmail.com>
2025-07-27 12:28:02 -0700

    feat: migrate from GitHub Pages to Cloudflare Workers
    - Replace Pages resources with Worker script and routes in Terraform
    - Add simple static asset serving Worker script
    - Update GitHub Actions to deploy Workers with assets
    - Add wrangler.toml configuration for Workers deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,55 +2,21 @@ data "cloudflare_zone" "bendrucker_me" {
   zone_id = var.cloudflare_zone_id
 }
 
-resource "cloudflare_pages_project" "bendrucker_me" {
-  account_id        = var.cloudflare_account_id
-  name              = "bendrucker-me"
-  production_branch = "master"
-
-  source {
-    type = "github"
-    config {
-      owner                         = "bendrucker"
-      repo_name                     = "bendrucker.me"
-      production_branch             = "master"
-      pr_comments_enabled           = true
-      deployments_enabled           = true
-      production_deployment_enabled = true
-      preview_deployment_setting    = "all"
-      preview_branch_includes       = ["*"]
-    }
-  }
-
-  build_config {
-    build_command       = "npm run build"
-    destination_dir     = "dist"
-    root_dir            = ""
-    web_analytics_tag   = ""
-    web_analytics_token = ""
-  }
-
-  deployment_configs {
-    production {
-      environment_variables = {}
-      secrets              = {}
-    }
-    preview {
-      environment_variables = {}
-      secrets              = {}
-    }
-  }
+resource "cloudflare_worker_script" "bendrucker_me" {
+  account_id = var.cloudflare_account_id
+  name       = "bendrucker-me"
+  content    = file("${path.module}/../worker.js")
+  module     = true
 }
 
-resource "cloudflare_pages_domain" "bendrucker_me" {
-  account_id   = var.cloudflare_account_id
-  project_name = cloudflare_pages_project.bendrucker_me.name
-  domain       = "bendrucker.me"
+resource "cloudflare_worker_route" "bendrucker_me" {
+  zone_id     = var.cloudflare_zone_id
+  pattern     = "bendrucker.me/*"
+  script_name = cloudflare_worker_script.bendrucker_me.name
 }
 
-resource "cloudflare_record" "bendrucker_me_cname" {
-  zone_id = var.cloudflare_zone_id
-  name    = "bendrucker.me"
-  content = cloudflare_pages_project.bendrucker_me.subdomain
-  type    = "CNAME"
-  proxied = true
+resource "cloudflare_worker_route" "bendrucker_me_www" {
+  zone_id     = var.cloudflare_zone_id
+  pattern     = "www.bendrucker.me/*"
+  script_name = cloudflare_worker_script.bendrucker_me.name
 }
