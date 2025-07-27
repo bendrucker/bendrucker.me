6ad49b191db26db52eb2b5d34e2e265d21393002
Ben Drucker <bvdrucker@gmail.com>
2025-07-27 12:45:03 -0700

    refactor: use data source to lookup zone by name
    - Remove cloudflare_zone_id variable
    - Use data.cloudflare_zone.bendrucker_me.id for zone references
    - Simplifies Terraform Cloud setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 data "cloudflare_zone" "bendrucker_me" {
-  zone_id = var.cloudflare_zone_id
+  name = "bendrucker.me"
 }
 
 resource "cloudflare_worker_script" "bendrucker_me" {
@@ -10,13 +10,13 @@ resource "cloudflare_worker_script" "bendrucker_me" {
 }
 
 resource "cloudflare_worker_route" "bendrucker_me" {
-  zone_id     = var.cloudflare_zone_id
+  zone_id     = data.cloudflare_zone.bendrucker_me.id
   pattern     = "bendrucker.me/*"
   script_name = cloudflare_worker_script.bendrucker_me.name
 }
 
 resource "cloudflare_worker_route" "bendrucker_me_www" {
-  zone_id     = var.cloudflare_zone_id
+  zone_id     = data.cloudflare_zone.bendrucker_me.id
   pattern     = "www.bendrucker.me/*"
   script_name = cloudflare_worker_script.bendrucker_me.name
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,3 @@ variable "cloudflare_account_id" {
   type        = string
   description = "Cloudflare account ID"
 }
-
-variable "cloudflare_zone_id" {
-  type        = string
-  description = "Cloudflare zone ID for bendrucker.me"
-}
