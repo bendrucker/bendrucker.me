5c14fcb7318b07029b54749744f26c4ccbe6c02b
Ben Drucker <bvdrucker@gmail.com>
2025-07-27 14:05:33 -0700

    refactor: use 'this' for resource names instead of bendrucker_me
    - Use 'this' for zone, script resources that don't need specific names
    - Use 'apex' and 'www' for routes that do need distinction
    - Update all references in outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,22 @@
-data "cloudflare_zone" "bendrucker_me" {
+data "cloudflare_zone" "this" {
   name = "bendrucker.me"
 }
 
-resource "cloudflare_workers_script" "bendrucker_me" {
+resource "cloudflare_workers_script" "this" {
   account_id = var.cloudflare_account_id
   name       = "bendrucker-me"
   content    = file("${path.module}/../worker.js")
   module     = true
 }
 
-resource "cloudflare_workers_route" "bendrucker_me" {
-  zone_id     = data.cloudflare_zone.bendrucker_me.id
+resource "cloudflare_workers_route" "apex" {
+  zone_id     = data.cloudflare_zone.this.id
   pattern     = "bendrucker.me/*"
-  script_name = cloudflare_workers_script.bendrucker_me.name
+  script_name = cloudflare_workers_script.this.name
 }
 
-resource "cloudflare_workers_route" "bendrucker_me_www" {
-  zone_id     = data.cloudflare_zone.bendrucker_me.id
+resource "cloudflare_workers_route" "www" {
+  zone_id     = data.cloudflare_zone.this.id
   pattern     = "www.bendrucker.me/*"
-  script_name = cloudflare_workers_script.bendrucker_me.name
+  script_name = cloudflare_workers_script.this.name
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "worker_script_name" {
-  value = cloudflare_workers_script.bendrucker_me.name
+  value = cloudflare_workers_script.this.name
 }
 
 output "zone_id" {
-  value = data.cloudflare_zone.bendrucker_me.id
+  value = data.cloudflare_zone.this.id
 }
 
 output "zone_name" {
-  value = data.cloudflare_zone.bendrucker_me.name
+  value = data.cloudflare_zone.this.name
 }
