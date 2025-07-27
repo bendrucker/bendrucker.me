7c5fc45aa2a2fe7d946d7c02dde1767c4f8165de
Ben Drucker <bvdrucker@gmail.com>
2025-07-27 14:42:04 -0700

    remove apex redirect - will be handled in separate repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,14 +9,6 @@ resource "cloudflare_workers_script" "this" {
   module     = true
 }
 
-resource "cloudflare_record" "apex_redirect" {
-  zone_id = data.cloudflare_zone.this.id
-  name    = "bendrucker.me"
-  content = "www.bendrucker.me"
-  type    = "CNAME"
-  proxied = true
-}
-
 resource "cloudflare_workers_route" "www" {
   zone_id     = data.cloudflare_zone.this.id
   pattern     = "www.bendrucker.me/*"
