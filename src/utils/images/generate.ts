ce1268b80ead9a68fe71130ba6f542eea4391e7f
Ben Drucker <bvdrucker@gmail.com>
2025-08-21 18:22:39 -0700

    convert uint array

--- src/utils/images/generate.ts
+++ src/utils/images/generate.ts
@@ -13,7 +13,7 @@ async function svgBufferToPngBuffer(svg: string) {
   }
   const resvg = new Resvg(svg);
   const pngData = resvg.render();
-  return pngData.asPng();
+  return new Uint8Array(pngData.asPng());
 }
 
 export async function generateOgImageForPost(post: CollectionEntry<"blog">) {
